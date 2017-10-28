09e0cd5929db1e3f6964ef2b0089f09ab59e94a4
umar <umar2345@gmail.com>
2017-10-28 07:06:25 +0000

    committing a project

---
+++ src/main/java/com/digital/lync/App.java
@@ -0,0 +1,13 @@
+package com.digital.lync;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
